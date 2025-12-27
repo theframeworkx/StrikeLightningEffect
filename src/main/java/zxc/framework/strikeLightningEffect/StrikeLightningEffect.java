d20fa74aa7f2cbe12376d95d8acc77d2fb5ea83a
framework <dmitriyaviaseils@gmail.com>
2025-12-27 17:01:45 +0300

    Update StrikeLightningEffect.java
    Added code optimization

--- src/main/java/zxc/framework/strikeLightningEffect/StrikeLightningEffect.java
+++ src/main/java/zxc/framework/strikeLightningEffect/StrikeLightningEffect.java
@@ -1,11 +1,21 @@
 package zxc.framework.strikeLightningEffect;
 
+import org.bukkit.entity.Entity;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.PlayerDeathEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public final class StrikeLightningEffect extends JavaPlugin {
-
+public final class StrikeLightningEffect extends JavaPlugin implements Listener {
     @Override
     public void onEnable() {
-        getServer().getPluginManager().registerEvents(new Events(), this);
+        getServer().getPluginManager().registerEvents(this, this);
+    }
+
+    @EventHandler
+    public void onPlayerDeath(PlayerDeathEvent event) {
+        Entity entity;
+        (entity = event.getEntity()).getWorld().strikeLightningEffect(entity.getLocation());
     }
 }
+
